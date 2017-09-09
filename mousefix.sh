9bd97af6a899812da659318f2ed29f68dc135058
Adrian Harabula <adrian.harabula@gmail.com>
2017-09-09 23:43:58 +0300

    add libinput driver support

--- mousefix.sh
+++ mousefix.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
 # Deactivate mouse acceleration
-sudo tee /usr/share/X11/xorg.conf.d/90-mouse.conf <<EOF
+# sudo tee /usr/share/X11/xorg.conf.d/90-mouse.conf <<EOF
+# Section "InputClass"
+#     Identifier "mouse"
+#     MatchIsPointer "on"
+#     Option "AccelerationProfile" "-1"
+#     Option "AccelerationScheme" "none"
+# EndSection
+# EOF
+
+# no mouse acceleration with libinput
+# in ubuntu 16.04 install libinput with
+# sudo apt install xserver-xorg-input-libinput-hwe-16.04
+sudo tee /usr/share/X11/xorg.conf.d/90-mouse-libinput.conf <<EOF
 Section "InputClass"
-    Identifier "mouse"
-    MatchIsPointer "on"
-    Option "AccelerationProfile" "-1"
-    Option "AccelerationScheme" "none"
+	Identifier "My Mouse"
+	Driver "libinput"
+	MatchIsPointer "yes"
+	Option "AccelProfile" "flat"
 EndSection
 EOF
 
-# Mouse pooling rate
+# Set mouse pooling rate
+# 1 = 1000Hz
+# 2 = 500Hz
+# 4 = 250Hz
+# 8 = 125Hz
+# 10 = 100Hz (Default) 
 sudo tee /etc/modprobe.d/usbhid.conf <<EOF
 options usbhid mousepoll=1
 EOF
